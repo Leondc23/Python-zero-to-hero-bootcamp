00532718566080bc334613c9d9c20c1ed8623a6d
Leondc23 <leondc23@outlook.com>
2026-06-09 21:45:27 +0100

    Add files via upload
    my first python code after completing section 3

---
+++ grade_calculator.py
@@ -0,0 +1,4 @@
+grades = [78, 52, 91, 62, 72]
+print(min(grades)," is your lowest grade") # using the min() function to find the lowest number in the list
+print(max(grades)," is your highest grade") #using the max() function to find the highest number in the list
+print(sum(grades)/5, " is your overall average") #sum adds all the values in the list. they are divided by 5 as that is the total number of grades.
